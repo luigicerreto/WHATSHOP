f7ef9150f0fdf634c5225d9798d8d12eade66a98
skizzotime <luigi.cerreto@live.it>
2017-02-08 18:08:54 +0100

    Conversione
    Convertiti tutti i file in PDF

--- WHATSHOP/WebContent/jdbc/Popolamento.sql
+++ WHATSHOP/WebContent/jdbc/Popolamento.sql
@@ -19,26 +19,26 @@ insert into NEGOZIO_ONLINE values ('6','Fai da te','supporto@faidate.it','098765
 
 insert into PRODOTTI values ('1','Fifa 17','49.90', '1','10','img/prod/fifa.jpg','6','5','Gioco di calcio');
 insert into PRODOTTI values ('2','NBA 2K17','59.90', '1','10','img/prod/nba.jpg','6','5','Gioco di Basket');
-insert into PRODOTTI values ('3','GTA 5','49.90', '1','10','img/prod/gta.jpg','6','5','Gioco da pazzi');
+insert into PRODOTTI values ('3','GTA 5','49.90', '1','10','img/prod/gta.jpg','6','5','Gioco vita reale');
 
 insert into PRODOTTI values ('4','Analisi','30.90', '1','10','img/prod/analisi.jpg','5','4','Libro di matematica');
-insert into PRODOTTI values ('5','Architettura degli elaboratori','50.00', '1','10','img/prod/arch.jpg','5','4','Architettura degli elaboratori si studia il MIMPS');
-insert into PRODOTTI values ('6','Matematica','20.90', '1','10','img/prod/mat.jpg','5','4','libro di matematica superiori');
+insert into PRODOTTI values ('5','Architettura degli elaboratori','50.00', '1','10','img/prod/arch.jpg','5','4','Architettura degli elaboratori si studia il MIPS');
+insert into PRODOTTI values ('6','Matematica','20.90', '1','10','img/prod/mat.jpg','5','4','Libro di matematica ');
 
 insert into PRODOTTI values ('7','Cintura pelle Hermes','749.99', '1','10','img/prod/hermes.jpg','4','3','Cintura abbigliamento');
-insert into PRODOTTI values ('8','Sciarpa','549.99', '1','10','img/prod/sciarpa.jpg','4','3','se ti fa male la gola');
-insert into PRODOTTI values ('9','Scarpe','350.00', '1','10','img/prod/scarpe.jpg','4','3','se vuoi uscire le devi mettere');
+insert into PRODOTTI values ('8','Sciarpa','549.99', '1','10','img/prod/sciarpa.jpg','4','3','Sciarpa di lana');
+insert into PRODOTTI values ('9','Scarpe','350.00', '1','10','img/prod/scarpe.jpg','4','3','Comode per il passeggio');
 
-insert into PRODOTTI values ('10','Aspirapolvere','900.90', '1','10','img/prod/asp.jpg','3','2','se hai la camera sporca(Marco)');
-insert into PRODOTTI values ('11','Lavasciuga','600.90', '1','10','img/prod/lavas.jpg','3','2','prima lava e poi asciuga');
-insert into PRODOTTI values ('12','Scopa elettrica 2 in 1','1049.90', '1','10','img/prod/scopa.jpg','3','2','Scopa in modo elettrico');
+insert into PRODOTTI values ('10','Aspirapolvere','900.90', '1','10','img/prod/asp.jpg','3','2','Se hai la camera sporca');
+insert into PRODOTTI values ('11','Lavasciuga','600.90', '1','10','img/prod/lavas.jpg','3','2','Una delle migliori nel mercato');
+insert into PRODOTTI values ('12','Scopa elettrica','1049.90', '1','10','img/prod/scopa.jpg','3','2','Di nuova generazione');
 
-insert into PRODOTTI values ('13','Motosega','300.90', '1','10','img/prod/motos.jpg','2','6','Motocicletta ma e\' una sega');
-insert into PRODOTTI values ('14','Taglia erba','250.90', '1','10','img/prod/tagliae.jpg','2','6','taglia l erba e\' facile');
-insert into PRODOTTI values ('15','Sega','23.90', '1','10','img/prod/sega.jpg','2','6','Tanta sega');
+insert into PRODOTTI values ('13','Motosega','300.90', '1','10','img/prod/motos.jpg','2','6','Per il tuo fai da te');
+insert into PRODOTTI values ('14','Taglia erba','250.90', '1','10','img/prod/tagliae.jpg','2','6','Per il tempo libero');
+insert into PRODOTTI values ('15','Sega','23.90', '1','10','img/prod/sega.jpg','2','6','Per il fai da te');
 
-insert into PRODOTTI values ('16','iphone 7 128GB','959.00', '1','10','img/prod/iphone7.jpg','1','1','chiami e invii sms');
-insert into PRODOTTI values ('17','MacBook Pro','1600.90', '1','10','img/prod/macp.jpg','1','1','viaggi nel web');
-insert into PRODOTTI values ('18','MacBook Air','1049.90', '1','10','img/prod/maca.jpg','1','1','il pc che vola');
+insert into PRODOTTI values ('16','Iphone 7 128GB','959.00', '1','10','img/prod/iphone7.jpg','1','1','Top di gamma');
+insert into PRODOTTI values ('17','MacBook Pro','1600.90', '1','10','img/prod/macp.jpg','1','1','Pc Apple');
+insert into PRODOTTI values ('18','MacBook Air','1049.90', '1','10','img/prod/maca.jpg','1','1','Pc Apple');
 
 
